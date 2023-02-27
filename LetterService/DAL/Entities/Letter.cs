283e07abde3b2f75e4e8bc2b44a3b447ff991153
Svyatoslav <svyatyar2@gmail.com>
2023-02-27 17:44:11 +0300

    [ADD] entities

---
+++ LetterService/DAL/Entities/Letter.cs
@@ -0,0 +1,9 @@
+﻿namespace LetterService.DAL.Entities;
+
+public class Letter
+{
+    public int Id { get; set; }
+    public DateTime PostTime { get; set; }
+    public string Message { get; set; }
+    public virtual ICollection<User> Users { get; } = new List<User>();
+}
---
+++ LetterService/DAL/Entities/LetterServiceDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LetterService.DAL.Entities;
+
+public partial class LetterServiceDbContext : DbContext
+{
+    public virtual DbSet<Letter> Letters { get; set; }
+    public virtual DbSet<User> Users { get; set; }
+
+    public LetterServiceDbContext(DbContextOptions<LetterServiceDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasMany(u => u.Letters).WithMany(l => l.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserLetter",
+                    r => r.HasOne<Letter>().WithMany()
+                        .HasForeignKey("LetterId"),
+                    l => l.HasOne<User>().WithMany()
+                        .HasForeignKey("UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "LetterId").IsClustered(false);
+                        j.ToTable("UserLetter");
+                    });
+        });
+        modelBuilder.Entity<Letter>(entity =>
+            entity.Property(l => l.PostTime).HasColumnType("datetie"));
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
