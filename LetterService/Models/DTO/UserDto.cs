35a6820e2b9167081b36efe5dd73e86a1df727d8
Svyatoslav <svyatyar2@gmail.com>
2023-03-01 15:14:06 +0300

    [ADD] email validation

--- LetterService/Models/DTO/UserDto.cs
+++ LetterService/Models/DTO/UserDto.cs
@@ -1,7 +1,10 @@
-﻿namespace LetterService.Models.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LetterService.Models.DTO;
 
 public class UserDto
 {
+    [EmailAddress]
     public required string Email { get; set; }
     public required string Password { get; set; }
 }
