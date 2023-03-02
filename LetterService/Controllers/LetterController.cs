2214753b06490d827356b62c1e51456b91326f9b
Svyatoslav <svyatyar2@gmail.com>
2023-03-02 10:26:25 +0300

    [ADD]  get user letters endpoint

--- LetterService/Controllers/LetterController.cs
+++ LetterService/Controllers/LetterController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using LetterService.DAL.Entities;
+using LetterService.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LetterService.Controllers;
 [Route("api/user/{userId}/letter")]
@@ -8,10 +12,25 @@ namespace LetterService.Controllers;
 [Authorize(Roles = "user, admin")]
 public class LetterController : ControllerBase
 {
+    private readonly LetterServiceDbContext _context;
+    private readonly IMapper _mapper;
+
+    public LetterController(LetterServiceDbContext context, IMapper mapper) =>
+        (_context, _mapper) = (context, mapper);
+
     [HttpGet]
-    public async Task<ActionResult> GetAllLettersAsync()
+    public async Task<ActionResult> GetAllLettersAsync(
+        [FromRoute] int userId    
+    )
     {
-        return Ok();
+        var letters = await _context.Letters
+            .Where(l => l.UserId == userId)
+            .OrderBy(l => l.PostTime)
+            .ToListAsync();
+
+        var lettersDto = _mapper.Map<IEnumerable<LetterDto>>(letters);
+
+        return Ok(lettersDto);
     }
 
     [HttpPost]
@@ -19,4 +38,27 @@ public class LetterController : ControllerBase
     {
         return Ok();
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateLetterAsync(
+        [FromBody] LetterDto model
+    )
+    {
+        var letter = await _context.Letters.FirstOrDefaultAsync(l => l.Id == model.Id);
+        if (letter is null)
+        {
+            return NotFound(new { message = $"Letter with id {model.Id} Not Foud" });
+        }
+
+        
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteLetterAsync()
+    {
+        return Ok();
+    }
+
+
 }
