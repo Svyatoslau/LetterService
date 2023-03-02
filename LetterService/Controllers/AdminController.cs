2e931d108cd163ebeaf89fc7085dd5a1a17ff1f3
Svyatoslav <svyatyar2@gmail.com>
2023-03-02 10:25:48 +0300

    [ADD] admin controller

--- LetterService/Controllers/AdminController.cs
+++ LetterService/Controllers/AdminController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using LetterService.DAL.Entities;
+using LetterService.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LetterService.Controllers;
 [Route("api")]
@@ -8,9 +12,29 @@ namespace LetterService.Controllers;
 [Authorize(Roles ="admin")]
 public class AdminController : ControllerBase
 {
+    private readonly LetterServiceDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AdminController(LetterServiceDbContext context, IMapper mapper) =>
+        (_context, _mapper) = (context, mapper);
+
     [HttpGet("user/letter")]
     public async Task<ActionResult> GetAllUsersLettersAsync()
     {
-        return Ok();
+        // подумать как лучше сделать
+        var letters = await _context.Users
+            .GroupJoin(
+                _context.Letters,
+                u => u.Id,
+                l => l.UserId,
+                (user, letters) => new
+                {
+                    User = _mapper.Map<UserDto>(user),
+                    Letters = _mapper.Map<IEnumerable<LetterDto>>(letters)
+                }
+            )
+            .ToListAsync();
+
+        return Ok(letters);
     }
 }
