6644c33673515cb422c67196b1fad0f6bcc7db35
Svyatoslav <svyatyar2@gmail.com>
2023-03-06 18:02:35 +0300

    [ADD] main-user-page component

---
+++ letter-view/src/app/main-user-page/main-user-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-user-page',
+  templateUrl: './main-user-page.component.html',
+  styleUrls: ['./main-user-page.component.css']
+})
+export class MainUserPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
