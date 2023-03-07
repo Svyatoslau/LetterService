fc106c7a9dd19ec96476cdd95983a6ca8c73be27
Svyatoslav <svyatyar2@gmail.com>
2023-03-07 15:15:52 +0300

    [ADD] letter module

---
+++ letter-view/src/app/letter/letter.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LetterDetailComponent } from './letter-detail/letter-detail.component';
+import { LetterListComponent } from './letter-list/letter-list.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { DateTimePickerModule } from '../date-time-picker/date-time-picker.module';
+import {MatListModule} from '@angular/material/list';
+import { LetterListItemComponent } from './letter-list-item/letter-list-item.component';
+import { LetterUserComponent } from './letter-user/letter-user.component';
+import {MatSelectModule} from '@angular/material/select'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    DateTimePickerModule,
+    MatListModule,
+    MatSelectModule
+  ],
+  declarations: [
+    LetterDetailComponent,
+    LetterListComponent,
+    LetterListItemComponent,
+    LetterUserComponent
+  ],
+  exports: [
+    LetterDetailComponent,
+    LetterListComponent,
+    LetterListItemComponent,
+    LetterUserComponent
+  ]
+})
+export class LetterModule { }
